bba07c7a595ebd17c46f2fa1a2bbf562707d4cc4
Christine Taylor <ctaylo84@my.smccd.edu>
2013-05-17 11:58:51 -0700

    validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,5 @@ class Url < ActiveRecord::Base
   attr_accessible :comment, :name, :student_id, :url
   belongs_to :student
   # add validation so that only URLs that end with herokuapp.com are accepted.
+  validates :url, :format => {:with => /^http:\/\/.*.herokuapp.com\/?$/}
 end
